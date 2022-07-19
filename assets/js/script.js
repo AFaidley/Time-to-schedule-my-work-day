88db69ff39f9ae95bd43bd69f8eccd8770963165
AFaidley <alviva.faidley@gmail.com>
2022-07-19 13:58:01 -0600

    Added local storage getItems to retrieve any saved data and adjusted html accordingly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,38 @@
-// Prints time at the top, 'Do' adds the th in 19th for exmaple, hh' makes it 12hr time and the 'a' adds AM or PM 
+// Global scope vars
+// console.log(new Date().getHours())
+var currenthour = new Date().getHours();
+
+// Prints time at the top, 'Do' adds the th in 19th for exmaple, hh' makes it 12hr time and the 'a' adds AM or PM
 setInterval(() => {
   var currentTime = moment().format("dddd MMMM Do YYYY hh:mm:ss a");
   $("#current-time").text(currentTime);
 }, 1000);
 
-// console.log(new Date())
+// Changes the color of the blocks by assigning a class to them according to the hour
+function schedulerColor() {
+  for (let i = 9; i < 18; i++) {
+    if (now > i) {
+      $(`#time-${i}`).addClass("past");
+    } else if (now == i) {
+      $(`#time-${i}`).addClass("present");
+    } else {
+      $(`#time-${i}`).addClass("future");
+    }
+  }
+}
+
+// Gets any saved data from local storage
+$("#time-9 .time-block").val(localStorage.getItem("time-9"));
+$("#time-10 .time-block").val(localStorage.getItem("time-10"));
+$("#time-11 .time-block").val(localStorage.getItem("time-11"));
+$("#time-12 .time-block").val(localStorage.getItem("time-12"));
+$("#time-13 .time-block").val(localStorage.getItem("time-13"));
+$("#time-14 .time-block").val(localStorage.getItem("time-14"));
+$("#time-15 .time-block").val(localStorage.getItem("time-15"));
+$("#time-16 .time-block").val(localStorage.getItem("time-16"));
+$("#time-17 .time-block").val(localStorage.getItem("time-17"));
+
+// A function to listen for the save button being clicked
 
 
+// Call functions
